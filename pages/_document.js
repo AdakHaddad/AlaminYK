49efd16489a8c2e8db897a46bf2154f37231b96d
AdakHaddad <muhammadmuqtada@gmail.com>
2023-08-20 13:22:10 +0700

    Update SEO + Google SEO HTML

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,20 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content=" Angkatan Muda Islam Nusantara Yogyakarta  "
+        />
+        <meta
+          name="keywords"
+          content="Organisasi, Kepemudaaan, Alawiyyin, Yogyakarta, Alamin, Al-amin, Rabithah Alawiyah"
+        />
+        <meta name="author" content="Alamin " />
         <link rel="shortcut icon" href="/images/favicon.ico" />
+        {/* Add other meta tags as needed for SEO optimization */}
+        <title>Your Page Title</title>
       </Head>
       <body>
         <Nav />
